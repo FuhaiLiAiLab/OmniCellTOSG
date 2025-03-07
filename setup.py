5d6796035d346d873d567e2b20df637de2f9992c
Tim Xu <13861170214@163.com>
2025-03-07 14:11:17 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="celltosg",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "torch", "re", "os"],
+    install_requires=["numpy", "pandas", "torch", "os"],
     python_requires=">=3.9",
 )
