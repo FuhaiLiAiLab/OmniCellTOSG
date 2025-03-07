3c8492c7491c54628c0c63d0855fd9acbd4eb645
Tim Xu <13861170214@163.com>
2025-03-07 14:08:07 -0600

    update structure

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="celltosg",
     version="0.1.0",
-    py_modules=["train", "dataset", "utils", "pretrain", "train-gnn", "data_loader"],
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "torch", "re", "os"],
     python_requires=">=3.9",
 )
