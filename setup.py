b439add53febc0aa39ad8227cf08ede5eda64866
Tim Xu <13861170214@163.com>
2025-06-18 21:21:41 -0500

    update code

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="celltosg_loader",
     version="0.1.1",
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "torch", "os", ],
+    install_requires=["numpy", "pandas", "torch"],
     python_requires=">=3.9",
 )
